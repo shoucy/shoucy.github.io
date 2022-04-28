b640fb0a47db855f63ebca68f839c23e62aa70f4
shoucy <shouchengyu@outlook.com>
2022-04-28 08:31:41 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,32 @@
+const { viteBundler } = require('@vuepress/bundler-vite')
+const { defaultTheme } = require('vuepress')
+const { searchPlugin } = require('@vuepress/plugin-search')
+
 module.exports = {
   lang: 'zh-CN',
   title: 'Ideal',
   description: '希望这个博客见证我成为更好的人',
 
-  themeConfig: {
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
+
+  theme: defaultTheme({
     logo: '/favicon.ico',
-  },
+  }),
 
   plugins: [
-    [
-      '@vuepress/plugin-search',
-      {
-        maxSuggestions: 10,
-        locales: {
-          '/': {
-            placeholder: 'Search',
-          },
-          '/zh/': {
-            placeholder: '搜索',
-          },
+    searchPlugin({
+      maxSuggestions: 10,
+      locales: {
+        '/': {
+          placeholder: 'Search',
         },
-      },
-    ],
+        '/zh/': {
+          placeholder: '搜索',
+        },
+      }
+    })
   ],
 }
---
+++ docs/java/maven.md
@@ -0,0 +1,8 @@
+
+
+## maven资源查询网站
+
+1. http://search.maven.org/
+
+2. http://mvnrepository.com/
+3. http://maven.outofmemory.cn/
